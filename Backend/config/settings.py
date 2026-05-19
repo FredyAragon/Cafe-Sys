60831e38039d7d55b73914707b988bb8cbae68ef
DARSEN12 <faragonc@unsa.edu.pe>
2026-05-19 13:30:30 -0500

    Probando docker y docker-compose

--- Backend/config/settings.py
+++ Backend/config/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'NAME': 'cafeteriadb',
         'USER': 'postgres',
         'PASSWORD': '1234',
-        'HOST': 'localhost',
+        'HOST': 'db',
         'PORT': '5432',
     }
 }
