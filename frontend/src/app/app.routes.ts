4ad59ba8467344757b6d411726c037e9ee014ded
DiegoMAC0263 <darceco@unsa.edu.pe>
2026-06-20 22:38:40 -0500

    agrega register a routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -39,7 +39,10 @@ export const routes: Routes = [
     path: 'login',
     loadComponent: () => import('./pages/login/login').then(m => m.LoginComponent)
   },
-
+  {
+    path: 'register',
+    loadComponent: () => import('./pages/register/register').then(m => m.RegisterComponent)
+  },
   // Ruta fallback para URLs no encontradas
   { path: '**', redirectTo: 'tienda' }
 ];
